ae335a5e8a1413e658a39010d21909e0241fa276
marc-bhunu <macbhunu@gmail.com>
2026-04-24 11:18:16 +0200

    feature: completed the insights service

---
+++ insights-service/src/main/java/com/marcuswhocodes/insights_service/InsightsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.marcuswhocodes.insights_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsightsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsightsServiceApplication.class, args);
+	}
+
+}
