738b7ee3093a0c097006c1b7a1b3c98a611ff8d3
marc-bhunu <macbhunu@gmail.com>
2026-04-28 15:57:20 +0200

    feat: completed circuit

--- api-gateway/src/main/java/com/marcuswhocodes/api_gateway/route/DeviceServiceRoutes.java
+++ api-gateway/src/main/java/com/marcuswhocodes/api_gateway/route/DeviceServiceRoutes.java
@@ -1,11 +1,15 @@
 package com.marcuswhocodes.api_gateway.route;
 
 
+import org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
+
+import java.net.URI;
 
 import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.uri;
 import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
@@ -19,6 +23,19 @@ public class DeviceServiceRoutes {
         return route("device-service")
                 .route(RequestPredicates.path("/api/v1/device/**"), http())
                 .before(uri("http://localhost:8081"))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker(
+                        "deviceServiceCircuitBreaker",
+                        URI.create("forward:/deviceServiceFallbackRoute")
+                ))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> deviceServiceFallBackRoute() {
+        return  route("deviceServiceFallbackRoute")
+                .route(RequestPredicates.path("/deviceServiceFallbackRoute"),
+                        request -> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE)
+                                .body("Device Service Is Unavailable"))
                 .build();
     }
 }
--- api-gateway/src/main/java/com/marcuswhocodes/api_gateway/route/IngestionServiceRoutes.java
+++ api-gateway/src/main/java/com/marcuswhocodes/api_gateway/route/IngestionServiceRoutes.java
@@ -1,11 +1,15 @@
 package com.marcuswhocodes.api_gateway.route;
 
 
+import org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
+
+import java.net.URI;
 
 import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.uri;
 import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
@@ -19,6 +23,19 @@ public class IngestionServiceRoutes {
         return route("ingestion-service")
                 .route(RequestPredicates.path("/api/v1/ingestion/**"), http())
                 .before(uri("http://localhost:8082"))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker(
+                        "ingestionServiceCircuitBreaker",
+                        URI.create("forward:/ingestionServiceFallbackRoute")
+                ))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> ingestionServiceFallBackRoute() {
+        return  route("ingestionServiceFallbackRoute")
+                .route(RequestPredicates.path("/ingestionServiceFallbackRoute"),
+                        request -> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE)
+                                .body("Ingestion Service Is Unavailable"))
                 .build();
     }
 }
